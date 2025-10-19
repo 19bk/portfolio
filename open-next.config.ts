537202e49df5a2b1c98953639e3fd6c9b8846cbe
Bernard <bernardkibathi91@gmail.com>
2025-10-19 22:44:30 +0300

    Simplify OpenNext config

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,14 +1,12 @@
 import type { OpenNextConfig } from "@opennextjs/cloudflare";
 
 const config: OpenNextConfig = {
-  output: ".open-next",
   cloudflare: {
     entry: {
       kind: "prerendered",
       include: ["/"],
-      exclude: []
-    }
-  }
+    },
+  },
 };
 
 export default config;
